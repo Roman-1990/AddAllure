9fb1fedb51dcd30e3cbb22e957ecbeffc35e57c3
Roman <pcuxman@yandex.ru>
2021-06-03 13:00:07 +0300

    Три проверки в одном файле 1) Чистый Selenide 2) Лямбда шаги через step (name, () -> {}) 3) Шаги с аннотацией @Step.

---
+++ src/test/java/gitHubAllure/AllureReportsTest.java
@@ -0,0 +1,85 @@
+package gitHubAllure;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.*;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static io.qameta.allure.Allure.step;
+
+public class AllureReportsTest {
+
+    private static final String BASE_URL = "https://github.com";
+    private static final String SEARCH = "allure";
+    private static final String REPOSITORY = "allure-framework/allure2";
+    private static final int ISSUE_NUMBER = 1294;
+
+    //Лямбда шаги через step
+    @Test
+    //дополнительная информация для Allure отчета
+    @Feature("Issues")
+    @Owner("Roman")
+    @Story("Создание Issues")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value = "Github", url = "https://github.com")
+    @DisplayName("Тест на проверку создания Issue")
+    public void testStepIssueSearch() {
+        step("Открываем страницу", (s) -> {
+            s.parameter("Url", BASE_URL);
+            open(BASE_URL);
+        });
+        step("Ищем репозиторий" + SEARCH, (s) -> {
+            s.parameter("repository", SEARCH);
+            $(".header-search-input").click();
+            $(".header-search-input").sendKeys(SEARCH);
+            $(".header-search-input").submit();
+        });
+        step("Переходим в репозиторий" + REPOSITORY, (s) -> {
+            s.parameter("repository", REPOSITORY);
+            $(By.linkText("allure-framework/allure2")).click();
+        });
+        step("Открываем таб Issues в репозитории", () ->
+                $(withText("Issues")).click()
+        );
+        step("Проверяем что Issues с номером" + ISSUE_NUMBER + "существует", (s) -> {
+            s.parameter("number", ISSUE_NUMBER);
+            $(withText("#1294")).should(Condition.exist);
+        });
+
+    }
+
+    //Selenid тест
+    @Test
+    public void testSelenideIssue() {
+        //создание лога
+        SelenideLogger.addListener("allure", new AllureSelenide().screenshots(false));
+
+        open(BASE_URL);
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(SEARCH);
+        $(".header-search-input").submit();
+        $(By.linkText("allure-framework/allure2")).click();
+        $(withText("Issues")).click();
+        $(withText("#" + ISSUE_NUMBER)).should(Condition.exist);
+
+    }
+
+    //Шаги с аннотацией @Step
+    private WebSteps steps = new WebSteps();
+
+    @Test
+    public void testAnnotatedStepsIssue() {
+        steps.openThePage();
+        steps.lookingRepository("allure");
+        steps.goRepository("allure-framework/allure2");
+        steps.openIssuesTabRepository();
+        steps.checkIssuesNumberExists(1294);
+
+    }
+}
---
+++ src/test/java/gitHubAllure/WebSteps.java
@@ -0,0 +1,44 @@
+package gitHubAllure;
+
+import com.codeborne.selenide.Condition;
+import io.qameta.allure.Step;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Selectors.withText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class WebSteps {
+
+    @Step("Открываем страницу")
+    public void openThePage() {
+        open("https://github.com");
+
+    }
+
+    @Step("Ищем репозиторий {Search}")
+    public void lookingRepository(String Search) {
+        $(".header-search-input").click();
+        $(".header-search-input").sendKeys(Search);
+        $(".header-search-input").submit();
+
+    }
+
+    @Step("Переходим в репозиторий {repository}")
+    public void goRepository(String repository) {
+        $(By.linkText(repository)).click();
+
+    }
+
+    @Step("Открываем таб Issues в репозитории")
+    public void openIssuesTabRepository() {
+        $(withText("Issues")).click();
+
+    }
+
+    @Step("Проверяем что Issues с номером {number} существует")
+    public void checkIssuesNumberExists(int number) {
+        $(withText("#" + number)).should(Condition.exist);
+
+    }
+}
